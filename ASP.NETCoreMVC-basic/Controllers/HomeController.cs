2989239d8be546b01b81444b00c5becd3da2c9e5
jw <cjw0515@naver.com>
2018-06-09 23:33:53 +0900

    no message

--- ASP.NETCoreMVC-basic/Controllers/HomeController.cs
+++ ASP.NETCoreMVC-basic/Controllers/HomeController.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MyApp.Models;
+using ASP.NETCoreMVC_basic.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace MyApp.wwwroot.Controllers
+namespace ASP.NETCoreMVC_basic.wwwroot.Controllers
 {
     public class HomeController : Controller
     {
@@ -26,8 +26,18 @@ namespace MyApp.wwwroot.Controllers
 
         //http post 값을 받는 역할을 한다.
         [HttpPost] 
+        /*[ValidateAntiForgeryToken] */ // 유저가 서버로 폼 정보를 보낼 때 이 정보가 유저에게 제공된 폼 정보가 맞는지 토큰 비교, 크로스 사이트 스크립팅 예방책 https://docs.microsoft.com/ko-kr/aspnet/core/security/anti-request-forgery?view=aspnetcore-2.1
         public IActionResult Student(Student model)
         {
+            //유효성 검사 
+            if (ModelState.IsValid)
+            {
+                //model 데이터 저장 로직
+            }
+            else
+            {
+                //에러 로직
+            }
             return View();
         }
     }
--- ASP.NETCoreMVC-basic/Models/Student.cs
+++ ASP.NETCoreMVC-basic/Models/Student.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyApp.Models
+namespace ASP.NETCoreMVC_basic.Models
 {
     public class Student
     {
 
         //자바와는 달리 이런식으로 해주면 자동으로 게터세터가 만들어진다.
         //prop + tab + tab
-        [BindNever] //name을 바인드 하지 않음.
+        //[BindNever] 
+        //name을 바인드 하지 않음.
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [Range(15,70)]
         public int Age { get; set; }
+        [Required, MinLength(5)]
         public String Country { get; set; }
     }
 }

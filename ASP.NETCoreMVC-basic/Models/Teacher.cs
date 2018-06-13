f53862aa9e6fc7be709463c9c432e1a91d65d613
최종원 <cjw0515@naver.com>
2018-06-13 23:46:28 +0900

    teacher 모델 추가

---
+++ ASP.NETCoreMVC-basic/Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ASP.NETCoreMVCbasic.Models
+{
+    public class Teacher
+    {
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+};

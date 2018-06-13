d081ea3028a2414fdea294158c139991f80303b9
최종원 <cjw0515@naver.com>
2018-06-13 23:46:57 +0900

    student, teacher을 같이 보기 위한 뷰 모델 추가

---
+++ ASP.NETCoreMVC-basic/ViewModels/StudentTeacherViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using ASP.NETCoreMVC_basic.Models;
+using ASP.NETCoreMVCbasic.Models;
+
+namespace ASP.NETCoreMVCbasic.ViewModels
+{
+    public class StudentTeacherViewModel
+    {
+        public Student Student { get; set; }
+        public List<Teacher> Teachers { get; set; }
+    }
+}

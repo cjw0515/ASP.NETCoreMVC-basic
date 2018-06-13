20cb01ff9ce43deb35c0a3df1643a1386de223d2
최종원 <cjw0515@naver.com>
2018-06-13 23:47:51 +0900

    no message

--- ASP.NETCoreMVC-basic/Controllers/HomeController.cs
+++ ASP.NETCoreMVC-basic/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ASP.NETCoreMVC_basic.Models;
+using ASP.NETCoreMVCbasic.Models;
+using ASP.NETCoreMVCbasic.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,7 +16,16 @@ namespace ASP.NETCoreMVC_basic.wwwroot.Controllers
         // GET: /<controller>/
         public IActionResult Student()
         {
-            return View();
+            List<Teacher> teachers = new List<Teacher>(){
+                new Teacher() { Name = "세종대왕", Class = "한글"}
+            };
+
+            var viewModel = new StudentTeacherViewModel()
+            {
+                Student = new Student(),
+                Teachers = teachers
+            };
+            return View(viewModel);
         }
 
 
@@ -27,7 +38,7 @@ namespace ASP.NETCoreMVC_basic.wwwroot.Controllers
         //http post 값을 받는 역할을 한다.
         [HttpPost] 
         /*[ValidateAntiForgeryToken] */ // 유저가 서버로 폼 정보를 보낼 때 이 정보가 유저에게 제공된 폼 정보가 맞는지 토큰 비교, 크로스 사이트 스크립팅 예방책 https://docs.microsoft.com/ko-kr/aspnet/core/security/anti-request-forgery?view=aspnetcore-2.1
-        public IActionResult Student(Student model)
+        public IActionResult Student(StudentTeacherViewModel model)
         {
             //유효성 검사 
             if (ModelState.IsValid)
